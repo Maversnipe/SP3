ab0e0976eabb5aa283bc6c12112a0d733bc80132
Wafieqa AZ <maguu.samurai@gmail.com>
2017-08-17 16:58:31 +0800

    irrklang ^^

--- DM2212_Physics/Physics/Source/SceneBase.cpp
+++ DM2212_Physics/Physics/Source/SceneBase.cpp
@@ -6,6 +6,7 @@
 #include "Application.h"
 #include "Utility.h"
 #include "LoadTGA.h"
+#include "SoundEngine.h"
 
 #include <sstream>
 
@@ -129,6 +130,10 @@ void SceneBase::Init()
 	BlockList[GEO_METAL] = MeshBuilder::GenerateCube("Metalblock", Color(1.f, 1.f, 1.f), 1.f);
 	BlockList[GEO_BRICK] = MeshBuilder::GenerateCube("Brickblock", Color(1.f, 0.f, 0.f), 1.f);
 
+	//Load sound (test)
+	CSoundEngine::GetInstance()->Init();
+	CSoundEngine::GetInstance()->AddSound("test", "Sound//test.mp3");
+
 	bLightEnabled = false;
 }
 
@@ -155,6 +160,7 @@ void SceneBase::Update(double dt)
 	}
 
 	fps = (float)(1.f / dt);
+
 }
 
 void SceneBase::RenderText(Mesh* mesh, std::string text, Color color)
