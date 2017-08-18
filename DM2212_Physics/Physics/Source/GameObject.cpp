74aa4cb1ee0170799e7ca654f076fa71f435701a
Wafieqa AZ <maguu.samurai@gmail.com>
2017-08-18 10:33:13 +0800

    Some textures, changed direction of the tiles also :,)

--- DM2212_Physics/Physics/Source/GameObject.cpp
+++ DM2212_Physics/Physics/Source/GameObject.cpp
@@ -4,7 +4,7 @@
 GameObject::GameObject(GAMEOBJECT_TYPE typeValue, BLOCK_TYPE Btyp) 
 	: type(typeValue),
 	scale(1, 1, 1),
-	dir(0, 1, 0),
+	dir(1, 0, 0),
 	active(false),
 	mass(1.f),
 	momentOfInertia(1.0f),
