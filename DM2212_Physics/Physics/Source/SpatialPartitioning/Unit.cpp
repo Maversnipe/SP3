e0b3195615431da1a1db2c862f8901659d62d476
Lucas Nguyen <lucas.nguyen123@gmail.com>
2017-08-22 13:24:23 +0800

    no message

--- DM2212_Physics/Physics/Source/SpatialPartitioning/Unit.cpp
+++ DM2212_Physics/Physics/Source/SpatialPartitioning/Unit.cpp
@@ -1,8 +1,11 @@
 #include "Unit.h"
+#include "Grid.h"
 
 Unit::Unit(Grid* grid, Vector3 pos_) 
-	//: m_grid(grid)
-	//, pos(pos_)
+	: m_grid(grid)
+	, pos(pos_)
+	, next_(NULL)
+	, prev_(NULL)
 {
 }
 
